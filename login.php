0a250ba3b50d38c8b2562a9092e7d939d1f48dac
thilini.yashodha.demel <thilini.yashodha.demel@gmail.com>
2022-11-03 17:06:33 +0530

    Commit 05

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+
+include "db_conn.php";
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
 
@@ -19,7 +22,24 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     header("Location: index.php?error=Password is Required");
     exit();
   }else {
-    echo "Valid input";
+    $sql = "SELECT * FROM consultant WHERE username='$uname' AND passowrd='$pass'";
+
+    $result = mysqli_query($conn,$sql);
+      if($result === FALSE) {
+        die(mysqli_error($conn));
+      }
+    $row = mysqli_fetch_assoc($result);
+    if ($row['username'] === $uname && $row['passowrd'] === $pass) {
+      $_SESSION['username'] = $row['username'];
+      $_SESSION['con_id'] = $row['con_id'];
+      $_SESSION['con_f_name'] = $row['con_f_name'];
+      header("Location: con_home.php");
+      exit();
+    }
+    else {
+      header("Location: index.php?error=Incorrect User Name or Password");
+      exit();
+    }
   }
 }
 
