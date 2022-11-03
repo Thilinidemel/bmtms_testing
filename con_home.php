a9ff6fbb656c6931ed3e96857b6a85c575ee5a86
thilini.yashodha.demel <thilini.yashodha.demel@gmail.com>
2022-11-03 17:07:25 +0530

    Commit 05

---
+++ con_home.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if (isset($_SESSION['con_id']) && isset($_SESSION['username'])) {
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!--give page title-->
+    <title>Consultant Page</title>
+    <!--attach css file-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <h1>Hello <?php echo $_SESSION['con_name']; ?></h1>
+  <a href="logout.php" class="logout">Logout</a>
+</body>
+</html>
+
+<?php
+}
+else {
+  header("Location: index.php");
+  exit();
+}
+?>
